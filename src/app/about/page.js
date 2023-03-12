f296c5150040505c08c72edf5b8c28231cd47646
Deimos Karkee <deimoskarrkey@outlook.com>
2023-03-12 20:44:03 +1100

    created navbar component and navbaritem card implemented underline acc to param

---
+++ src/app/about/page.js
@@ -0,0 +1,26 @@
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        This is a IMDB clone website where you can search for different
+        movies.You can also see the trending movies and top rated movies.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        provident itaque voluptas odio voluptate nesciunt quam ab veritatis
+        consequuntur. Esse quas odit magnam tenetur natus quidem cum repellendus
+        vel amet!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur quae,
+        laudantium magnam repudiandae quam ad id? Modi dolore architecto
+        laboriosam veniam, cumque placeat qui, quibusdam ex praesentium aut
+        blanditiis aliquam possimus illo rerum sequi eligendi facilis cum quod
+        porro ducimus?
+      </p>
+    </div>
+  );
+}
+
+export default About;
